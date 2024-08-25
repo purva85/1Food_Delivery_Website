c9c353c8277f1375eab8c74a3a64539fb16263c2
purva12 <143676523+purva85@users.noreply.github.com>
2024-08-25 20:42:28 +0530

    Add files via upload

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <footer>
+<p className="text-center">
+    Binndast - Food Delivery 2024-25. All Copy Rights Reserved by Binndast...
+</p>
+    </footer>
+    </>
+  );
+}
